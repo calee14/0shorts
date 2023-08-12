73007f7898e2142afe0a90f0019a60553dda206a
calee14 <caplee1000@gmail.com>
2023-08-12 00:25:10 -0700

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  
+  const [titile, setTitle] = useState();
+
   return (
     <div className="App">
       <header className="App-header">
