3cfe1a04d29116a3bcbbae7c1e6fbd3e3f23009c
calee14 <caplee1000@gmail.com>
2023-08-20 23:17:40 -0700

    add background checks

--- public/background.js
+++ public/background.js
@@ -3,4 +3,13 @@ const webRequestCallback = async (details) => {
     console.log(details.url);
 };
 const filter = {urls: []};
+
 chrome.webRequest.onCompleted.addListener(webRequestCallback, filter);
+
+const onBeforeWebRequestCallback = async (details) => {
+    console.log('here before')
+    console.log(details.url);
+    blockingResponse = {cancel: true}
+};
+
+chrome.webRequest.onBeforeRequest.addListener(onBeforeWebRequestCallback, filter);
